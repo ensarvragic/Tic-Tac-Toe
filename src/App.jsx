c0a0f5130bd118fc324125df010b4dd9a2dbade0
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2023-12-22 14:25:46 +0100

    styled main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <p>Coming soon...</p>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-simbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
     </>
-  )
+  );
 }
 
-export default App
+export default App;
