d5abb3451e3b388e03affac5a7e91b8385f38909
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-01 03:11:23 +0100

    polishing app

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,25 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
+function deriveWinner(gameBoard, players) {
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquare = gameBoard[combination[0].row][combination[0].column];
+    const secondSquare = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquare = gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquare &&
+      firstSquare === secondSquare &&
+      firstSquare === thirdSquare
+    ) {
+      winner = players[firstSquare];
+    }
+  }
+  return winner;
+}
+
 function App() {
   const [players, setPlayers] = useState({
     X: "Player 1",
@@ -42,22 +61,7 @@ function App() {
     gameBoard[row][col] = player;
   }
 
-  let winner;
-
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquare = gameBoard[combination[0].row][combination[0].column];
-    const secondSquare = gameBoard[combination[1].row][combination[1].column];
-    const thirdSquare = gameBoard[combination[2].row][combination[2].column];
-
-    if (
-      firstSquare &&
-      firstSquare === secondSquare &&
-      firstSquare === thirdSquare
-    ) {
-      winner = players[firstSquare];
-    }
-  }
-
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -82,7 +86,7 @@ function App() {
     setPlayers((prevPlayers) => {
       return {
         ...prevPlayers,
-        [symbol]: newName
+        [symbol]: newName,
       };
     });
   }
