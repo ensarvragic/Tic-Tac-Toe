1bf7279ef654f7a8b779f003c12b1f0732ab41ea
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-01-01 02:34:17 +0100

    finished draw modal

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     //setActivePlayer((curActive) => (curActive === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
@@ -83,7 +85,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {winner && <GameOver winner={winner}/>}
+        {(winner || hasDraw)&& <GameOver winner={winner}/>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
